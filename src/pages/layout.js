5ba3ae82ab026d64aa6d809d8ad42b9fcd7bc7d0
Guilherme Paiva <guilherme@pointmedia.com.br>
2024-03-27 15:54:01 -0300

    Finalizada resolução 1920

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,10 +1,5 @@
 import '../styles/globals.css';
 
-export const metadata = {
-  title: "re.animate",
-  description: "Transforme dias de trabalho em minutos com o re.animate",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html>
